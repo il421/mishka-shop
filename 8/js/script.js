ba6cb4079595bf5bbf1404eff54a44de593bf24f
Keks <keks@htmlacademy.ru>
2016-10-07 18:46:24 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/script.js
@@ -0,0 +1,35 @@
+/*Menu*/
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+/*Module Form*/
+var link = document.querySelector(".modal-form__link");
+var popup = document.querySelector(".modal-form");
+var bg = document.querySelector(".modal-wrapper");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-form--show");
+  bg.classList.add("modal-wrapper--show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal-form--show")) {
+      popup.classList.remove("modal-form--show");
+      bg.classList.remove("modal-wrapper--show");
+    }
+  }
+});
